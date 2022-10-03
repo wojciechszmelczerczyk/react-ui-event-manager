4436277757af383accb5ebeaf4c3290cd01a294a
Wojciech <szmelczerczykwojciech@gmail.com>
2022-10-03 07:20:58 +0200

    refresh token service included

---
+++ src/components/EventPrompt.tsx
@@ -0,0 +1,11 @@
+const EventPrompt = () => {
+  return (
+    <div className='flex justify-center items-center  min-h-screen w-screen bg-transparent bg-opacity-0'>
+      <div className='absolute top-1/2 left-1/2 -translate-x-2/4 -translate-y-2/4 z-4 h-64 w-64 bg-white rounded-lg shadow-lg'>
+        hello
+      </div>
+    </div>
+  );
+};
+
+export default EventPrompt;
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -4,5 +4,14 @@ import axiosInstance from "../api/axiosInstance";
 export const register = async (userCredentials: any) =>
   await axiosInstance.post("/user", userCredentials);
 
+// authenticate user with jsonwebtoken
 export const login = async (userCredentials: any) =>
   await axiosInstance.post("/user/authenticate", userCredentials);
+
+// get new access token, store new refresh token in db
+export const refreshToken = async (rt: any) =>
+  await axiosInstance.get("/user/refreshToken", {
+    headers: {
+      Authorization: `Bearer ${rt}`,
+    },
+  });
